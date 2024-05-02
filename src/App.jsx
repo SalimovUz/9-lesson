3687ccd54c87b566c329b890962ed73fa1ae1833
Mohigulfront-end-developer <moxiigul.isanova74@gmail.com>
2024-05-02 11:37:05 +0500

    mohigul

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import CommentUser from './Components/CommentUser'
 
 const App = () => {
   return (
     <div>
       <h1>Hello World</h1>
       <p>This</p>
+      <CommentUser/>
     </div>
   )
 }
